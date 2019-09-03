2ea51079d2dd1a0b8bde8a49b06ebb34560c3bfa
Andrey Kashapov <kashapovandrey@gmail.com>
2019-09-03 16:40:01 +0300

    feat: view projects

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   FaChevronCircleDown,
@@ -7,41 +7,46 @@ import {
   FaRegCalendar
 } from "react-icons/fa";
 
+import { useSelectedProjectValue } from "../../context/";
+import { Projects } from "../Projects";
+
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li>
+        <li data-testid="inbox" className="inbox">
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li>
+        <li data-testid="today" className="today">
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li>
+        <li data-testid="next_7" className="next_7">
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-
       <div className="sidebar__middle">
         <span>
           <FaChevronCircleDown />
         </span>
         <h2>Projects</h2>
       </div>
-
       <ul className="sidebar__projects">
         <li>Projects will be here.</li>
       </ul>
-
+      <Projects />
       Add projects Component.
     </div>
   );
